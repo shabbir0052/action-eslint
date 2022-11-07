a66d408537bb6eab56438b39b167b959a167f517
Shabbir Husain <shabbir.husain@csaa.com>
2022-11-07 13:26:28 -0800

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,53 @@
 module.exports = {
-  env: {
-    node: true,
-    commonjs: true,
-    es2021: true,
-  },
-  ignorePatterns: [
-    "**/UnitTest/*.js",
-    "/bin/*.js",
-    "/lib/*.js",
-    "**/logging/lib/*.js",
-    "**/logging/test/*.js",
-    "/.vscode/*.*",
-  ],
-  overrides: [
-    {
-      files: ["**/*.js"],
-      extends: ["airbnb-base", "prettier"],
-      plugins: ["prettier"],
-      parserOptions: {
-        ecmaVersion: "latest",
-      },
-      rules: {},
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
     },
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      plugins: ["@typescript-eslint"],
-      extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: ["./tsconfig.json"],
-      },
-    },
-  ],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
+
+    ignorePatterns: [
+        "**/UnitTest/*.js",
+        "/bin/*.js",
+        "/lib/*.js",
+        "**/logging/lib/*.js",
+        "**/logging/test/*.js",
+        "/.vscode/*.*",
     ],
-  },
-  settings: {
-    "import/core-modules": ["aws-sdk", "aws-xray-sdk-core"],
-  },
+    
+    overrides: [
+        {
+            files: ["**/*.js"],
+            extends: ["airbnb-base", "prettier"],
+            plugins: ["prettier"],
+            parserOptions: {
+                ecmaVersion: "latest",
+            },
+            rules: {
+                "prettier/prettier": [
+                    "error",
+                    {
+                        endOfLine: "auto",
+                    },
+                ],
+            }
+        },
+        {
+            files: ["**/*.ts", "**/*.tsx"],
+            plugins: ["@typescript-eslint"],
+            extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                project: ["./tsconfig.json"],
+            },
+        },
+    ],
+
+    rules: {
+        indent: ["error", 4],
+        quotes: ["error", "double"],
+        semi: ["error", "always"]
+    },
+    settings: {
+        "import/core-modules": ["aws-sdk", "aws-xray-sdk-core","snowflake-sdk"],
+    },    
 };
